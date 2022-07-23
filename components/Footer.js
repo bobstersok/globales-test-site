b83cf6ef21102eb0fc73a6f272d3843319de918c
Bobby Shackleton <73130406+bobstersok@users.noreply.github.com>
2022-07-24 00:35:29 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Part of the Globales Group
       </footer>
     </>
   )
